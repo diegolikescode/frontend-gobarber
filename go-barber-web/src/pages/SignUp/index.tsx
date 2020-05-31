6dec393222d1676207811c6874b1040d656b9238
Diego Prestes <diegoprestes.git@hotmail.com>
2020-05-31 15:32:29 -0300

    Nearly completed front

--- go-barber-web/src/pages/SignUp/index.tsx
+++ go-barber-web/src/pages/SignUp/index.tsx
@@ -1,10 +1,8 @@
-import React, { useRef, useCallback } from 'react';
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
-import { Form } from '@unform/web';
+import React, { useCallback, useRef } from 'react';
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
 import * as Yup from 'yup';
-
-import getValidationError from '../../utils/getValidationError';
 
 import { Container, Content, Background } from './styles';
 
@@ -12,8 +10,9 @@ import logo from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import getValidationError from '../../utils/getValidationError';
 
-const SignIn: React.FC = () => {
+const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
   const handleSubmit = useCallback(async (data: object) => {
@@ -25,7 +24,7 @@ const SignIn: React.FC = () => {
         email: Yup.string()
           .required('E-mail obrigatório.')
           .email('Digite um e-mail válido.'),
-        password: Yup.string().required('Senha obrigatória.'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
       });
 
       await schema.validate(data, {
@@ -40,13 +39,21 @@ const SignIn: React.FC = () => {
 
   return (
     <Container>
+      <Background />
+
       <Content>
         <img src={logo} alt="GoBarber" />
 
         <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Faça seu Logon</h1>
+          <h1>Faça seu Cadastro</h1>
 
-          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="name"
+            icon={FiUser}
+            type="text"
+            placeholder="Nome Usuário"
+          />
+          <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
 
           <Input
             name="password"
@@ -55,19 +62,16 @@ const SignIn: React.FC = () => {
             placeholder="Password"
           />
 
-          <Button type="submit">Entrar</Button>
-
-          <a href="forgot">Esqueci minha senha</a>
+          <Button type="submit">Cadastrar</Button>
         </Form>
 
         <a href="login">
-          <FiLogIn />
-          Criar conta
+          <FiArrowLeft />
+          Voltar para logon
         </a>
       </Content>
-      <Background />
     </Container>
   );
 };
 
-export default SignIn;
+export default SignUp;
