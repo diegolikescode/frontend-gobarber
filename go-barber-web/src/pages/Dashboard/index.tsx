1e85e1dea6e347ce89bb4cc621f885e9774a719a
DiegoPrestesGit <diegoprestes.git@hotmail.com>
2020-08-07 16:10:47 -0300

    doing great at reactJS

--- go-barber-web/src/pages/Dashboard/index.tsx
+++ go-barber-web/src/pages/Dashboard/index.tsx
@@ -1,5 +1,136 @@
 import React from 'react';
 
-const Dashboard: React.FC = () => <h1>XESQUEDALE</h1>;
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment,
+  Appointment,
+  Section,
+} from './styles';
+import logoImg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/auth';
+
+const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <span>Bem vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="http://localhost:3333/files/dc3d92ca67804e895f0c-xqdaleriven.jpg"
+                alt="Diegão"
+              />
+
+              <strong>Diegão</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="http://localhost:3333/files/dc3d92ca67804e895f0c-xqdaleriven.jpg"
+                  alt="Diegão"
+                />
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="http://localhost:3333/files/dc3d92ca67804e895f0c-xqdaleriven.jpg"
+                  alt="Diegão"
+                />
+              </div>
+            </Appointment>
+          </Section>
+          <Section>
+            <Section>
+              <strong>Tarde</strong>
+
+              <Appointment>
+                <span>
+                  <FiClock />
+                  08:00
+                </span>
+
+                <div>
+                  <img
+                    src="http://localhost:3333/files/dc3d92ca67804e895f0c-xqdaleriven.jpg"
+                    alt="Diegão"
+                  />
+                </div>
+              </Appointment>
+
+              <Appointment>
+                <span>
+                  <FiClock />
+                  08:00
+                </span>
+
+                <div>
+                  <img
+                    src="http://localhost:3333/files/dc3d92ca67804e895f0c-xqdaleriven.jpg"
+                    alt="Diegão"
+                  />
+                </div>
+              </Appointment>
+            </Section>
+          </Section>
+        </Schedule>
+        <Calendar />
+      </Content>
+    </Container>
+  );
+};
 
 export default Dashboard;
